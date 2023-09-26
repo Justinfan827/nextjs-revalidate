155a90ca16c88d5fc1705e043dc9ada9bd9457b1
Justin Fan <justinfan827@gmail.com>
2023-09-26 14:28:56 -0700

    wow

--- src/app/home/layout.jsx
+++ src/app/home/layout.jsx
@@ -6,7 +6,7 @@ export default function HomeLayout({ children }) {
   return (
     <div className="w-7xl mx-auto py-10 px-12">
       <h1 className="py-4">Main Layout with revalidate: {revalidate}</h1>
-      <div classname="outline outline-red-200 outline-2">{children}</div>
+      <div className="outline outline-red-200 outline-2">{children}</div>
     </div>
   );
 }
